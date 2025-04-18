5f8fea908a59576c983660563c49b4effc7011b3
Thomas-Brito <thomasbrito.tjb@gmail.com>
2025-04-18 00:32:29 -0300

    Atualização de qualidade

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
-class Modal {
-  constructor(modal, abrir, fechar) {
-    this.modal = modal;
-    this.abrir = abrir;
-    this.fechar = fechar;
+const documento = document.documentElement;
+const gerenciaTema = {
+  lightDark: document.querySelector(`input[name="tema"][value="light-dark"]`),
+  light: document.querySelector(`input[name="tema"][value="light"]`),
+  dark: document.querySelector(`input[name="tema"][value="dark"]`),
+  modal: document.getElementById(`modal-tema`),
+  abrirModal: () => { gerenciaTema.modal.style.display = `block`; },
+  fecharModal: () => { gerenciaTema.modal.style.display = `none`; },
+  aplicarTema: (tema) => {
+    documento.setAttribute(`data-theme`, tema);
+    localStorage.setItem(`tema`, tema);
+  },
+  mudarTema: function () {
+    if (this.lightDark.checked === true) {
+      this.aplicarTema(`light-dark`);
+    } else if (this.light.checked === true) {
+      this.aplicarTema(`light`);
+    } else if (this.dark.checked === true) {
+      this.aplicarTema(`dark`);
+    } else {
+      this.aplicarTema(`light-dark`);
+    }
+  },
+  recuperarTema: function () {
+    const temaSalvo = localStorage.getItem(`tema`);
+    switch (temaSalvo) {
+      case `light-dark`:
+        this.lightDark.checked = true;
+        documento.setAttribute(`data-theme`, `light-dark`);
+        break;
+      case `light`:
+        this.light.checked = true;
+        documento.setAttribute(`data-theme`, `light`);
+        break;
+      case `dark`:
+        this.dark.checked = true;
+        documento.setAttribute(`data-theme`, `dark`);
+        break;
+      default:
+        this.lightDark.checked = true;
+        documento.setAttribute(`data-theme`, `light-dark`);
+        break;
+    }
   }
 }
-
-const modalTema = new Modal();
-
-modalTema.modal = document.getElementById(`minha-modal`);
-
-modalTema.abrir = document.getElementById(`abrir-modal`).addEventListener(`click`, function() {
-  modalTema.modal.style.display = `block`;
-});
-
-modalTema.fechar = document.getElementById(`fechar-modal`).addEventListener(`click`, function() {
-  modalTema.modal.style.display = `none`;
-});
-
-modalTema.mudarTema = function(evento) {
-  const temaAtual = evento.target.value;
-  document.documentElement.setAttribute(`data-theme`, temaAtual);
-  localStorage.setItem(`tema`, temaAtual);
-};
-
-modalTema.botoes = document.querySelectorAll(`input[name="tema"]`);
-
-modalTema.botoes.forEach((botao) => {
-  botao.addEventListener(`change`, modalTema.mudarTema);
-});
-
-let temaSalvo = localStorage.getItem(`tema`);
-
-switch (temaSalvo) {
-  case `light-dark`:
-    document.querySelector(`input[name="tema"][value="light-dark"]`).checked = true;
-    document.documentElement.setAttribute(`data-theme`, `light-dark`);
-    break;
-  case `light`:
-    document.querySelector(`input[name="tema"][value="light"]`).checked = true;
-    document.documentElement.setAttribute(`data-theme`, `light`);
-    break;
-  case `dark`:
-    document.querySelector(`input[name="tema"][value="dark"]`).checked = true;
-    document.documentElement.setAttribute(`data-theme`, `dark`);
-    break;
-  default:
-    document.querySelector(`input[name="tema"][value="light-dark"]`).checked = true;
-    document.documentElement.setAttribute(`data-theme`, `light-dark`);
-    break;
-}
+document.getElementById(`abrir-modal`).addEventListener(`click`, () => gerenciaTema.abrirModal());
+document.getElementById(`fechar-modal`).addEventListener(`click`, () => gerenciaTema.fecharModal());
+document.querySelectorAll(`input[name="tema"]`).forEach(input => input.addEventListener(`change`, () => gerenciaTema.mudarTema()));
+gerenciaTema.recuperarTema();
