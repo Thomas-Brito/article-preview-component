761a0b10a85bd80ddc9a129ce05228f3cc3a6662
Thomas-Brito <thomasbrito.tjb@gmail.com>
2025-04-20 18:55:20 -0300

    Atualização de qualidade do código js

--- script.js
+++ script.js
@@ -1,49 +1,27 @@
 const documento = document.documentElement;
 const gerenciaTema = {
-  lightDark: document.querySelector(`input[name="tema"][value="light-dark"]`),
-  light: document.querySelector(`input[name="tema"][value="light"]`),
-  dark: document.querySelector(`input[name="tema"][value="dark"]`),
+  tema: document.querySelectorAll(`input[name="tema"]`),
   modal: document.getElementById(`modal-tema`),
-  abrirModal: () => { gerenciaTema.modal.style.display = `block`; },
-  fecharModal: () => { gerenciaTema.modal.style.display = `none`; },
+  abrirModal: function () { this.modal.style.display = `block`; },
+  fecharModal: function () { this.modal.style.display = `none`; },
   aplicarTema: (tema) => {
     documento.setAttribute(`data-theme`, tema);
     localStorage.setItem(`tema`, tema);
   },
-  mudarTema: function () {
-    if (this.lightDark.checked === true) {
-      this.aplicarTema(`light-dark`);
-    } else if (this.light.checked === true) {
-      this.aplicarTema(`light`);
-    } else if (this.dark.checked === true) {
-      this.aplicarTema(`dark`);
-    } else {
-      this.aplicarTema(`light-dark`);
-    }
-  },
   recuperarTema: function () {
     const temaSalvo = localStorage.getItem(`tema`);
-    switch (temaSalvo) {
-      case `light-dark`:
-        this.lightDark.checked = true;
-        documento.setAttribute(`data-theme`, `light-dark`);
-        break;
-      case `light`:
-        this.light.checked = true;
-        documento.setAttribute(`data-theme`, `light`);
-        break;
-      case `dark`:
-        this.dark.checked = true;
-        documento.setAttribute(`data-theme`, `dark`);
-        break;
-      default:
-        this.lightDark.checked = true;
-        documento.setAttribute(`data-theme`, `light-dark`);
-        break;
-    }
+    const temasValidos = Array.from(this.tema).map(input => input.value);
+    const temaAtual = temasValidos.includes(temaSalvo) ? temaSalvo : this.tema[0].value;
+    this.tema.forEach(tema => {
+      if (tema.value === temaAtual) {
+        tema.checked = true;
+        documento.setAttribute(`data-theme`, temaAtual);
+      }
+    });
+    localStorage.setItem(`tema`, temaAtual);
   }
 }
 document.getElementById(`abrir-modal`).addEventListener(`click`, () => gerenciaTema.abrirModal());
 document.getElementById(`fechar-modal`).addEventListener(`click`, () => gerenciaTema.fecharModal());
-document.querySelectorAll(`input[name="tema"]`).forEach(input => input.addEventListener(`change`, () => gerenciaTema.mudarTema()));
+gerenciaTema.tema.forEach(input => input.addEventListener(`change`, (evento) => gerenciaTema.aplicarTema(evento.target.value)));
 gerenciaTema.recuperarTema();
